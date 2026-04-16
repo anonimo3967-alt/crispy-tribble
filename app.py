b62d0b5e769a63d048e3a6d49ff1eda93a71ba84
anonimo3967-alt <anonimo3967@gmail.com>
2026-04-16 10:13:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,17 @@ st.title("Thragg got his aura redeemed")
 with st.sidebar:
   st.subheader("Propiedades del tablero")
 
-st.subheader("Dimensiones del tablero")
-canvas_width = st.slider("Ancho del tablero", 300, 700, 500, 50)
-canvas_height = st.slider("Alto del tablero", 200, 600, 300, 50)
+  st.subheader("Dimensiones del tablero")
+  canvas_width = st.slider("Ancho del tablero", 300, 700, 500, 50)
+  canvas_height = st.slider("Alto del tablero", 200, 600, 300, 50)
 
-drawing_mode = st.selectbox("Herramienta de dibujo:", ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),)
+  drawing_mode = st.selectbox("Herramienta de dibujo:", ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),)
 
-stroke_width = st.slider("Selecciona el ancho de línea", 1, 30, 15)
+  stroke_width = st.slider("Selecciona el ancho de línea", 1, 30, 15)
 
-stroke_color = st.color_picker("Color de trazo", "#FFFFFF")
+  stroke_color = st.color_picker("Color de trazo", "#FFFFFF")
 
-bg_color = st.color_picker("Color de fondo", "#000000")
+  bg_color = st.color_picker("Color de fondo", "#000000")
 
 canvas_result = st_canvas(
   fill_color="rgba(255, 165, 0, 0.3)",
